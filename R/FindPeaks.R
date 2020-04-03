d0ee322571185b1115e7b3841797491eb9308437
Dawsey <43346309+Dawsey@users.noreply.github.com>
2020-04-03 23:31:11 +0800

    Add files via upload
    Added code to allow for the use of null index values

--- R/FindPeaks.R
+++ R/FindPeaks.R
@@ -1,31 +1,59 @@
-#' show.peaks
+#' Show timeseries peak values above a threshold
 #'
 #' This function plots the peak values above the entered threshold argument.
 #'
-#' @param x Path to the input file
-#' @param threshold the value above which to calculate peaks
-#' @return A plot of the peaks at the specified threshold
+#' @param data a dataframe containing the measurements and optional index variables
+#' @param index column reference entered as a character variable to the indices. Indices are plotted on the x axis. Observations can be either a date or numeric format.
+#' @param values column reference entered as a character variable to the values against which the peak values are calculated. Observations ust be numeric format
+#' @param threshold a numeric value above which to calculate peaks.
+#' @param ... other arguments to pass to the plot function.
+#' @return A plot of the peaks at the specified threshold and Peak value data that can be assigned to an object
+#' @examples
+#' data(waves)
+#' show.peaks(data = waves,
+#'            index = "date_time",
+#'            values = "height_meters",
+#'            threshold = 1.3)
+#'
+#' # To show data without a date/time index
+#' show.peaks(data = waves,
+#'            values = "height_meters",
+#'            threshold = 1.3)
 #' @export
 
-show.peaks <- function(x, threshold = 0, ...) {
-  # This function requires an index value (i.e. dates/times) in column 1 and values (numeric) in column 2
+show.peaks <- function(data, index = NULL, values, threshold = 0, ...) {
+
+   if (!is.null(index)) {
+
+     x <- {{data}} %>%
+       select(index, {{values}})
+
+   } else {
+
+     x <- {{data}} %>%
+       mutate(index = as.numeric(row.names({{data}}))) %>%
+       select(index, {{values}})
+     row.names(x) <- NULL
+   }
 
   # Identify peaks
   pks <- which(diff(sign(diff(x[, 2], na.pad = FALSE)), na.pad = FALSE) < 0) + 1 # extract all peaks
-  # Specify a threshold for the peak identification
-  #threshold <- threshold
+
   # Subset the peaks based on the threshold
   x1pks <- subset(x[pks, ], x[pks, 2] > threshold)
 
-  # Plot the results
-  plot(x[,1:2], type="l", ...)
+  plot(x[, ], type="l", ...)
   points(x1pks, col="blue") # Show peaks above threshold
   abline(h = threshold, col = "red") # Show the threshold limit
-  text(x = quantile((x1pks[, 1]), 0.25), y = quantile(x1pks[, 2], 0.99), adj = c(0.5, 1), paste0("Threshold = ", threshold), srt=0, col = "red")
-  text(x = quantile((x1pks[, 1]), 0.25), y = quantile(x1pks[, 2], 0.99), adj = c(0.5, 3), paste0("# of Peaks = ", nrow(x1pks)), srt=0, col = "blue")
-  #points(x1pks,1:2], col="red") # Show all peaks
+
+  text(x = quantile((x1pks[, 1]), 0.25),
+       y = quantile(x1pks[, 2], 0.99),
+       adj = c(0.5, 1), paste0("Threshold = ", threshold), srt=0, col = "red")
+
+  text(x = quantile((x1pks[, 1]), 0.25),
+       y = quantile(x1pks[, 2], 0.99),
+       adj = c(0.5, 3), paste0("# of Peaks = ", nrow(x1pks)), srt=0, col = "blue")
+
   return(x1pks)
 }
 
-# devtools::create("myfirstpackage")
-# Use devtools::document() to prepare documentaiton for the package
